1688d2d578c822646957c963c234e67c716422f5
gitavk <3apa3ka3@gmail.com>
2015-07-25 11:25:20 +0300

    change folders.

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fcbproject.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fcbp.settings")
 
     from django.core.management import execute_from_command_line
 
