a3b467459fe6f3b2c2cb2a077fa6be01ae07d0d2
gitavk <3apa3ka3@gmail.com>
2015-07-25 16:34:29 +0300

    users serializers.

--- fcbp/urls.py
+++ fcbp/urls.py
@@ -1,10 +1,6 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
     # url(r'^$', 'fcbp.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/v1/users/', include('users.urls')),
 )
--- users/views.py
+++ users/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.contrib.auth import authenticate, login, logout
+
+from rest_framework import status, views
+from rest_framework.response import Response
+
+from .models import User
+
+
+class LogoutView(views.APIView):
+    """
+    Logout action.
+    """
+    def post(self, request, format=None):
+        logout(request)
+
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+
+class LoginView(views.APIView):
+    """
+    Login action.
+    """
+    def post(self, request, format=None):
+        data = json.loads(request.body)
+
+        username = data.get('username', None)
+        password = data.get('password', None)
+
+        account = authenticate(username=username, password=password)
+
+        if account is not None:
+            if account.is_active:
+                login(request, account)
+
+                serialized = AccountSerializer(account)
+
+                return Response(serialized.data)
+            else:
+                return Response({
+                    'status': 'Unauthorized',
+                    'message': 'This account has been disabled.'
+                }, status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            return Response({
+                'status': 'Unauthorized',
+                'message': 'Username/password combination invalid.'
+            }, status=status.HTTP_401_UNAUTHORIZED)
