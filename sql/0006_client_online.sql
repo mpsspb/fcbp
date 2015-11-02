b1657a0262f7d057085996433f8b9b92897f97fb
gitavk <3apa3ka3@gmail.com>
2015-11-02 03:46:48 +0300

    Aqua online for multi clients.

--- sql/0006_client_online.sql
+++ sql/0006_client_online.sql
@@ -7,15 +7,15 @@ SELECT row_number() over() as id, client_id, date, product
 FROM (
     SELECT cc.client_id, ucc.date, 'clubcard' as product
     FROM clients_clientclubcard cc
-     LEFT JOIN clients_useclientclubcard ucc
+     INNER JOIN clients_useclientclubcard ucc
         ON cc.id = ucc.client_club_card_id
     WHERE ucc.end IS NULL
 
     UNION ALL
 
     SELECT a.client_id, ua.date, 'aqua' as product
-    FROM clients_clientaquaaerobics a
-     LEFT JOIN clients_useclientaquaaerobics ua
+    FROM v_external_aqua a
+     INNER JOIN clients_useclientaquaaerobics ua
         ON a.id = ua.client_aqua_aerobics_id
     WHERE ua.end IS NULL
 
@@ -23,7 +23,7 @@ FROM (
 
     SELECT t.client_id, ut.date, 'ticket' as product
     FROM clients_clientticket t
-     LEFT JOIN clients_useclientticket ut
+     INNER JOIN clients_useclientticket ut
         ON t.id = ut.client_ticket_id
     WHERE ut.end IS NULL
 
@@ -31,7 +31,7 @@ FROM (
 
     SELECT p.client_id, up.date, 'personal' as product
     FROM clients_clientpersonal p
-     LEFT JOIN clients_useclientpersonal up
+     INNER JOIN clients_useclientpersonal up
         ON p.id = up.client_personal_id
     WHERE up.end IS NULL
     ) online
